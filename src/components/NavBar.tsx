85d0324dd9d0b1a5e123fbc63d2932168bd2269f
Florentin Monteil <flo.monteil@gmail.com>
2025-04-14 00:12:29 +0200

    create toggle component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,14 @@
+import "../styles/NavBar.css";
+import FloLogo from "../assets/logosvg.svg";
+import CustomToggle from "./CustomToggle";
+
 export default function NavBar() {
-  return <div>HelloWorld !</div>;
+  return (
+    <div className="navbar-container">
+      <div className="navbar-left-container">
+        <img className="navbar-logo" src={FloLogo} alt="Logo" />
+        <CustomToggle onChange={(checked) => console.log(checked)} />
+      </div>
+    </div>
+  );
 }
